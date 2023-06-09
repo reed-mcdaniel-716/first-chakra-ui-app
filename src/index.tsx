1438c8ac89ee9c2a84052c810d43e62f0ce62e7d
Reed McDaniel <reed.mcdaniel.716@gmail.com>
2023-06-09 08:44:03 -0400

    initial setup and prettier install

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// 1. import `ChakraProvider` component and extendTheme function
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+// Extend the theme to include custom colors, fonts, etc
+const brandColors = {
+  brand: {
+    plum: '#431E37',
+    creamsicle: '#FE9927',
+    dustyBlue: '#272A4D',
+    clay: '#5B2A06',
+    inky: '#10050B',
+    paper: '#F0EEE1',
+    palmSprings: '#DD735C',
+    slate: '#4E5B6E',
+    ash: '#9796A3',
+    ballet: '#E9C9AC',
+  },
+};
+
+const theme = extendTheme({ brandColors });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// 2. Wrap ChakraProvider at the root of your app
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
